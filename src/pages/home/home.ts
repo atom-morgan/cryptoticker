aa408bbbf417936c0db240717da6ba640b54bfcf
Adam Morgan <atom.morgan@gmail.com>
2018-01-03 21:58:20 -0500

    Add call to coinmarket service to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { CoinmarketProvider } from '../../providers/coinmarket/coinmarket';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  currencies: Array<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private coinMarket: CoinmarketProvider) {}
 
+  ngOnInit() {
+    this.coinMarket.get().subscribe(res => this.currencies = res);
+  }
+
+  currencySelected(id) {
+    console.log('currency ' + id + ' selected');
   }
 
 }
